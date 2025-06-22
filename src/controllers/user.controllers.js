5743c972f3bd5f0d66116a89e8be996537f99eaf
Eric <patinetakiller564@gmail.com>
2025-06-22 15:15:28 -0300

    me da miedo q se me borre todo

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,13 +1,50 @@
 import Usuario from "../models/user.models.js";
 
+//Obtener todos los Usuarios
+export const obtenerUsuario = async (req, res) => {
+  //Obtener a TODOS los Usuarios
+  try {
+    const usuario = await Usuario.findAll(req.body);
+    res.status(201).json(usuario);
+  } catch (Error) {
+    console.log("Error en OBTENER el usuario", Error);
+  }
+};
 
-
-
+//Crea los Usuarios
 export const crearUsuario = async (req, res) => {
+  //Verifación de parametros Nulos
+  const { name, password, email } = req.body;
+  if (name === "" || password === "" || email === "") {
+        throw new Error("No pueden haber parámetros nulos ");
+  }
 
+  
+  
   try {
     const usuario = await Usuario.create(req.body);
     res.status(201).json(usuario);
   } catch (Error) {
-    console.log("Error en crear el usuario", Error);
-}};
+    console.log("Error en CREAR el usuario", Error);
+  }
+};
+
+//Actualiza a los Usuarios
+export const actualizarUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.update(req.body);
+    res.status(201).json(usuario);
+  } catch (Error) {
+    console.log("Error en ACTUALIZAR el usuario", Error);
+  }
+};
+
+//Elimina a todos los Usuarios
+export const eliminarUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.destroy(req.body);
+    res.status(201).json(usuario);
+  } catch (Error) {
+    console.log("Error en ELIMINAR el usuario", Error);
+  }
+};
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,11 +1,23 @@
+//Acá se declara las caracteristicas quye va a tener la tabla, ya sea
+//string,allownull,etc
+
 import sequelize from "../config/database.js";
 import { DataTypes } from "sequelize";
 
-const Usuario = sequelize.define("Usuario",{
-    name:{type:DataTypes.STRING(50),allowNull:false},
-    email:{type:DataTypes.STRING(50),allowNull:false,unique:true,validate:{isEmail:true}},
-    password:{type:DataTypes.STRING(20),allowNull:false,validate:{len:[6,20]}},
-    isActive:{type:DataTypes.BOOLEAN,allowNull:false}
+//Decalramos la const Usuario = sequelize.define para acceder a los datatypes
+const Usuario = sequelize.define("Usuario", {
+  name: { type: DataTypes.STRING(50), allowNull: false },Number:false,
+  email: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+  password: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  isActive: { type: DataTypes.BOOLEAN, allowNull: false },
 });
 
 export default Usuario;
